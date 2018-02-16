ff2433d3fc883ce5f3e46c0d6aaf9b2bb73c05bc
Peter Spiro <peterspiro@users.noreply.github.com>
2018-02-16 14:09:49 -0800

    update setup

--- setup.sh
+++ setup.sh
@@ -4,5 +4,8 @@ UTIL_DIR=$(dirname $BASH_SOURCE)
 export UTIL_DIR
 
 files=( ".gitconfig" ".gitignore_global" ".vip" ".emacs" ".bash_profile")
-ln -s $UTIL_DIR/.gitignore_global ~
+for f in "${files}"
+do
+    ln -s $UTIL_DIR/.gitignore_global ~
+done
 
