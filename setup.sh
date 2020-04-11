b2c0ecc8fd0c7387f78a1ae084de604e4bd33c07
Peter Spiro <peterspiro@users.noreply.github.com>
2020-04-11 14:38:55 -0700

    include util config in global config

--- setup.sh
+++ setup.sh
@@ -2,9 +2,10 @@
 
 UTIL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-files=( ".gitconfig" ".gitignore_global" ".vip" ".emacs" ".bash_profile" ".bash_prompt")
+files=(".gitignore_global" ".vip" ".emacs" ".bash_profile" ".bash_prompt")
 for f in "${files[@]}"
 do
     ln -s $UTIL_DIR/$f ~
 done
 
+git config --global include.path $UTIL_DIR/.gitconfig
