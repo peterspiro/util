b323465b527aa587e4fc967898afbf4c4993f209
Peter Spiro <peterspiro@users.noreply.github.com>
2020-04-11 14:21:30 -0700

    more robust method to find directory

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-UTIL_DIR=$(dirname $BASH_SOURCE)
-export UTIL_DIR
+UTIL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 files=( ".gitconfig" ".gitignore_global" ".vip" ".emacs" ".bash_profile" ".bash_prompt")
 for f in "${files[@]}"
