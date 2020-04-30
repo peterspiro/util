662ffc806161f36ee81896338779c6555a99a189
Peter Spiro <peterspiro@users.noreply.github.com>
2020-04-29 19:54:06 -0700

    fix config commands for util dir

--- setup.sh
+++ setup.sh
@@ -7,7 +7,9 @@
 #!/usr/bin/bash -x
 #!/usr/bin/env bash
 
+
 UTIL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 
 files=(".gitignore_global" ".vip" ".emacs" ".bash_profile" ".bash_prompt")
 for f in "${files[@]}"
@@ -15,7 +17,11 @@ do
     ln -s $UTIL_DIR/$f ~
 done
 
+
 git config --global include.path $UTIL_DIR/.gitconfig
+
+
+cd $UTIL_DIR
 
 git config user.email peterspiro@users.noreply.github.com
 git config user.name "Peter Spiro"
